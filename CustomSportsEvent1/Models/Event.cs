baca760426f6732ef6dcd5600f4c3f377306b309
ahmedkrajcin <ahmedkrajcin@gmail.com>
2017-05-17 18:20:05 +0200

    models

---
+++ CustomSportsEvent1/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomSportsEvent1.Models
+{
+    public class Event
+    {
+        int eventID;
+        DateTime Date;
+        TimeSpan StartTime;
+        Place placeId;
+
+    }
+}
